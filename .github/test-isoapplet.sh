4c451dfeadc76215e39ac801f15d8f18eec6d37d
Claus Steuer <claus.steuer@swissbit.com>
2022-07-29 14:24:01 +0200

    improve output of isoapplet test on failure

--- .github/test-isoapplet.sh
+++ .github/test-isoapplet.sh
@@ -85,4 +85,7 @@ rm /tmp/ECprivKey.pem /tmp/ECpubKey.pem /tmp/data.bin /tmp/data.sig
 
 kill -9 $PID
 
-diff -u3 src/tests/p11test/isoapplet{_ref,}.json
+if ! diff -u3 src/tests/p11test/isoapplet.json src/tests/p11test/isoapplet_ref.json; then
+    echo "The output of p11test has changed (see diff above). If that is expected, update the reference file. Otherwise, fix the error."
+    exit 1
+fi
