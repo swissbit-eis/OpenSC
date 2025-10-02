ec082bfff69aa3e322d7aeae48c30e321b1f9f16
Zeren Lu <zeren.lu@swissbit.com>
2025-10-02 16:04:12 +0200

    add sm error check for revalidating pin

--- src/libopensc/pkcs15-sec.c
+++ src/libopensc/pkcs15-sec.c
@@ -156,7 +156,9 @@ static int use_key(struct sc_pkcs15_card *p15card,
 		if (revalidated_cached_pin)
 			/* only re-validate once */
 			break;
-		if (r == SC_ERROR_SECURITY_STATUS_NOT_SATISFIED) {
+		if (r == SC_ERROR_SECURITY_STATUS_NOT_SATISFIED || r == SC_ERROR_SM_NO_SESSION_KEYS) {
+			/* When secure messaging is enabled 6982 is mapped
+			 * to SC_ERROR_SM_NO_SESSION_KEYS */
 			r = sc_pkcs15_pincache_revalidate(p15card, obj);
 			if (r < 0)
 				break;
