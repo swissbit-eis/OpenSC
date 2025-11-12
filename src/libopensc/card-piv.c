8b5270feaaa5a3565beee3484f953b5303a6b1ee
David <david.nutz@swissbit.com>
2025-11-12 13:39:17 +0100

    Added missing management key algorithm handling

--- src/libopensc/card-piv.c
+++ src/libopensc/card-piv.c
@@ -415,6 +415,7 @@ typedef struct piv_private_data {
 	unsigned int pin_policy; /* from discovery */
 	unsigned int init_flags;
 	u8  csID; /* 800-73-4 Cipher Suite ID 0x27 or 0x2E */
+	u8 mgmt_key_alg;
 #ifdef ENABLE_PIV_SM
 	cipher_suite_t *cs; /* active cipher_suite */
 	piv_cvc_t sm_cvc;  /* 800-73-4:  SM CVC Table 15 */
@@ -852,6 +853,7 @@ static int piv_cache_internal_data(sc_card_t *card, int enumtag);
 static int piv_logout(sc_card_t *card);
 static int piv_match_card_continued(sc_card_t *card);
 static int piv_obj_cache_free_entry(sc_card_t *card, int enumtag, int flags);
+static int piv_process_management_key_algorithm(sc_card_t *card);
 
 #ifdef ENABLE_PIV_SM
 static void piv_inc(u8 *counter, size_t size);
@@ -5541,6 +5543,7 @@ static int piv_match_card_continued(sc_card_t *card)
 	/* TODO Dual CAC/PIV are bases on 800-73-1 where priv->pin_preference = 0. need to check later */
 	priv->logged_in = SC_PIN_STATE_UNKNOWN;
 	priv->pstate = PIV_STATE_MATCH;
+	priv->mgmt_key_alg = 0;
 
 #ifdef ENABLE_PIV_SM
 	memset(&card->sm_ctx, 0, sizeof card->sm_ctx);
@@ -5628,8 +5631,10 @@ static int piv_match_card_continued(sc_card_t *card)
                                                          swissbit_version_buf[2];
                                 sc_log(card->ctx, "Swissbit card->type=%d, r=0x%08x version=0x%08x", card->type, r, priv->swissbit_version);
                         }
+                        piv_process_management_key_algorithm(card);
         }
 
+        sc_log(card->ctx, "Management key algorithm is 0x%08x", priv->mgmt_key_alg);
         sc_debug(card->ctx,SC_LOG_DEBUG_MATCH, "PIV_MATCH card->type:%d r2:%d CI:%08x r:%d\n", card->type, r2, priv->card_issues, r);
 
 	 /* We now know PIV AID is active, test CCC object. 800-73-* say CCC is required */
@@ -5827,6 +5832,46 @@ err:
 	LOG_FUNC_RETURN(card->ctx, r);
 }
 
+/*
+ * Get the algorithm of the management key for admin operations.
+ */
+static int
+piv_process_management_key_algorithm(sc_card_t *card)
+{
+	int r;
+
+	sc_apdu_t apdu;
+	sc_format_apdu(card, &apdu,
+			SC_APDU_CASE_2_SHORT, // command with output data only
+			0xF7,				 // INS: GET METADATA Card Command
+			0x00,				 // P1
+			0x9B					// P2
+	);
+
+	unsigned char rbuf[13];
+	apdu.resp = rbuf;
+	apdu.resplen = sizeof(rbuf);
+	apdu.le = sizeof(rbuf);
+
+	r = sc_transmit_apdu(card, &apdu);
+	LOG_TEST_RET(card->ctx, r, "Transmit GET METADATA APDU failed");
+
+	r = sc_check_sw(card, apdu.sw1, apdu.sw2);
+	LOG_TEST_RET(card->ctx, r, "GET METADATA command failed");
+
+	size_t algorithm_tag_len;
+	const u8 *algorithm;
+	algorithm = sc_asn1_find_tag(card->ctx, apdu.resp, apdu.resplen, 0x01,
+			&algorithm_tag_len);
+	if (!algorithm || algorithm_tag_len != 1) {
+		sc_log(card->ctx, "Cannot get key management algorithm.");
+		return SC_ERROR_ASN1_OBJECT_NOT_FOUND;
+	}
+
+	piv_private_data_t *priv = PIV_DATA(card);
+	priv->mgmt_key_alg = *algorithm;
+	return SC_SUCCESS;
+}
 
 static int piv_init(sc_card_t *card)
 {
